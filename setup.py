a0bdab00d8a7f5e0c7fba9bd70456361ad98cc13
AshwinParanjape <ashwing.2005@gmail.com>
2018-10-05 17:03:09 -0700

    Preparatory commit for PyPI

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+        name = "meticulous",
+        version = "0.0.1",
+        author = "Ashwin Paranjape",
+        author_email = "ashwing.2005@gmail.com",
+        description = "A package to track the code used to run experiments and compare different experimental runs",
+        long_description = long_description,
+        url = "https://github.com/AshwinParanjape/meticulous",
+        packages = setuptools.find_packages(),
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
