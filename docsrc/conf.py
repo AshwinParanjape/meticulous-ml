49384e5420c9268c0dd651e6123771312a79fa8a
ashwinparanjape <ashwing.2005@gmail.com>
2020-11-30 02:06:55 -0800

    Added nojekyll

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -33,6 +33,7 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.todo',
     'sphinx.ext.napoleon',
+    'sphinx.ext.githubpages',
     'recommonmark',
     'sphinxcontrib.autoprogram',
 ]
