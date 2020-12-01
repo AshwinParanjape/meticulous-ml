313410d35e8e4591a8d0de437f0f2b88bf25630a
ashwinparanjape <ashwing.2005@gmail.com>
2020-11-30 18:59:19 -0800

    Removed unneccessary print statement

--- meticulous/summary_utils.py
+++ meticulous/summary_utils.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
 def informative_cols(dataframe):
-    print(dataframe.nunique(dropna=False)>1)
     return [c for c, v in dataframe.nunique(dropna=False).iteritems() if v > 1]
 
 def truncate_constant_cols(dataframe):
